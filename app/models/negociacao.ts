f3b63911cfd9418f91643357af68658b460af3fb
Maikon Panazzolo <maikon.panazzolo@gmail.com>
2023-01-16 19:52:27 -0300

    initial commit

---
+++ app/models/negociacao.ts
@@ -0,0 +1,17 @@
+export class Negociacao {
+    #data;
+    #valor;
+    #quantidade;
+
+    constructor(data, valor, quantidade) {
+        this.#data = data;
+        this.#valor = valor;
+        this.#quantidade = quantidade;
+    }
+
+    get data() { return this.#data; }
+
+    get valor() { return this.#valor; }
+
+    get quantidade() { return this.#quantidade; }
+}
